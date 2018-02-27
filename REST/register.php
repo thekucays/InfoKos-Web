e28a5d69d278e4bf28fb53c39f6fd3eef6ca7776
thekucays <thekucays@gmail.com>
2018-02-28 01:16:47 +0700

    added register with multipart form-data

--- REST/register.php
+++ REST/register.php
@@ -1,61 +1,95 @@
 <?php
+	/*
+		this one using multipart/form data
+		https://gist.github.com/no1k/8492575
+		https://stackoverflow.com/questions/30654310/post-variables-not-working-with-files-and-multipart-form-data
+	*/
 	require_once('../config/koneksi.php');
 	require_once('RESTconfig.php');
+	require_once('../config/upload.php');
 
-
-	// get parameter
-	$json_str = file_get_contents('php://input'); //https://davidwalsh.name/php-json
-	$request = json_decode($json_str, TRUE); //http://php.net/manual/en/function.json-decode.php
-
-	// parameter checking
-	$nama = array_key_exists('nama', $request) ? $request['nama'] : '';
-	$idno = array_key_exists('idno', $request) ? $request['idno'] : '';  // nomor ktp
-	$email = array_key_exists('email', $request) ? $request['email'] : '';
-	$alamat = array_key_exists('alamat', $request) ? $request['alamat'] : '';
-	$gender = array_key_exists('gender', $request) ? $request['gender'] : '';
-	$phone_number = array_key_exists('phone_number', $request) ? $request['phone_number'] : '';
-	$college = array_key_exists('college', $request) ? $request['college'] : '';
-	$password = array_key_exists('password', $request) ? $request['password'] : '';
-	$password_conf = array_key_exists('password_conf', $request) ? $request['password_conf'] : '';
+	// print_r($_FILES);
+	// echo $_POST['nama'];
 
 	// return values
 	$description = 'invalid parameter';
 	$status = 'nok';
 	$userid_registered = '';
 
-	// validate session
-	if($nama=='' || $idno=='' || $email=='' || $alamat=='' || $gender=='' || $phone_number=='' || $college=='' || $password=='' || $password_conf==''){
+	// parameter checking
+	if(!isset($_POST['nama']) || $_POST['nama']==''){
 		$description = 'invalid parameter';
-	} else{
-		if($password != $password_conf){
-			$description = 'password and password confirmation did not match';
-		}else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    		$description = 'invalid email address';
-		}else if($gender != 'm' && $gender != 'f'){
-			$description = 'invalid gender';
-		}else{
-			// check user already registered
-			$file_foto = 'logo.JPEG';
-			$file_type = pathinfo($file_foto, PATHINFO_EXTENSION);
-			$file_data = file_get_contents($file_foto);
-			$file_base64 = 'data:image/' . $file_type . ';base64,' . base64_encode($file_data);
-			$file_obj = json_encode(array("image"=>$file_base64));
+	}else if(!isset($_FILES['photo']) || $_FILES['photo']==''){
+		$description = 'invalid parameter';
+	}else if(!isset($_POST['idno']) || $_POST['idno']==''){
+		$description = 'invalid parameter';
+	}else if(!isset($_POST['email']) || $_POST['email']==''){
+		$description = 'invalid parameter';
+	}else if(!isset($_POST['alamat']) || $_POST['alamat']==''){
+		$description = 'invalid parameter';
+	}else if(!isset($_POST['gender']) || $_POST['gender']==''){
+		$description = 'invalid parameter';
+	}else if(!isset($_POST['phone_number']) || $_POST['phone_number']==''){
+		$description = 'invalid parameter';
+	}else if(!isset($_POST['college']) || $_POST['college']==''){
+		$description = 'invalid parameter';
+	}else if(!isset($_POST['password']) || $_POST['password']==''){
+		$description = 'invalid parameter';
+	}else if(!isset($_POST['password_conf']) || $_POST['password_conf']==''){
+		$description = 'invalid parameter';
+	}else if($_POST['gender'] != 'm' && $_POST['gender'] != 'f'){
+		$description = 'invalid parameter';
+	}else if($_POST['password'] != $_POST['password_conf']){
+		$description = 'invalid parameter';
+	}else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+		$description = 'invalid parameter';
+	}
 
-			// all OK
-			$status = "ok";
-			$description = 'Registrasi Berhasil! Silahkan cek email anda untuk melakukan konfirmasi';
+	// processing data
+	else{
+		// check already registered
+		$checkIsRegAlready = mysql_query("SELECT * FROM pelanggan WHERE email = '" . $_POST['email'] . "'");
+		$isregData = mysql_fetch_array($checkIsRegAlready);
+		$isReg = mysql_num_rows($checkIsRegAlready);
+		if($isReg != 0){
+			$description = 'Maaf User '. $isregData['nama'] .' ('. $isregData['email']. ') sudah terdaftar.';
+		}else{
+			// upload photo to server
+			$nama_file = $_FILES['photo']['name'];
+			$nama_file = UploadPhoto($nama_file);
+
+			// insert pelanggan data to DB
+			$insert = mysql_query("INSERT INTO pelanggan(nama,
+                                        email,
+                                        ktp,
+                                        password,
+                                        alamat,
+                                        jenis_kelamin,
+                                        no_hp,
+                                        kampus,
+                                        photo,
+                                        aktif)
+                                   VALUE('$_POST[nama]',
+                                        '$_POST[email]',
+                                        '$_POST[idno]',
+                                        '$_POST[password]',
+                                        '$_POST[alamat]',
+                                        '$_POST[gender]',
+                                        '$_POST[phone_number]',
+                                        '$_POST[college]',
+                                        '$nama_file',
+                                        '1')");
+	        if($insert){
+	        	$status = 'ok';
+	        	$description = 'Selamat User $_POST[nama] ($_POST[email]) berhasil didaftarkan.';
+	        }else{
+	        	$description = 'Maaf User $_POST[nama] ($_POST[email]) gagal didaftarkan.';
+	        }
 		}
 	}
 
 	// generate result
 	$result['status'] = $status;
 	$result['description'] = $description;
-	$result['status'] = $status;
 	echo json_encode($result);
-
-	// if($gender == 'm'){
-	// 	echo 'yes';
-	// }else{
-	// 	echo 'no';
-	// }
 ?>
